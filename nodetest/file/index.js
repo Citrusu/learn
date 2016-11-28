6212f39a455b3de3012b4a523e34c53a9d92f15b
Citrus <zhouyou421@126.com>
2016-11-28 09:53:50 +0800

    delet some

---
+++ nodetest/file/index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var files = ['a.txt','b.txt','c.txt'];
+
+for (var i = 0 ; i < files.length; i++){
+    fs.readFile(files[i] , 'utf-8' ,function(err , content){
+        //console.log(files[i] + ':' + content)
+        console.log(files);
+        console.log(i);
+        console.log(files[i]);
+    })
+}
