d2465d6e01d25aad04c2cf821facc16620ea8254
Citrus <zhouyou421@126.com>
2017-02-20 20:45:56 +0800

    update readme

---
+++ code/date/2017/0220/cout.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Citrus on 2017/2/20.
+ */
+var chefu = 1;
+var ma = 1;
+var ren = 5;
+var koudai = ren * 5;
+var damao = koudai * 5;
+var xiaomao = damao * 5;
+var laoshu = xiaomao * 5;
+console.log((chefu * 2) + (ma * 4) + (ren * 2) + (damao * 4) + (xiaomao * 4) + (laoshu * 4));
