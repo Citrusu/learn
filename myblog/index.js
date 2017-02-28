c57741dcf55d0dda9b069e83fbfe418016b91f46
Citrus <zhouyou421@126.com>
2017-02-28 21:58:08 +0800

    add signup

--- myblog/index.js
+++ myblog/index.js
@@ -45,6 +45,12 @@ app.use(session({
 //flash 中间件，用来显示通知
 app.use(flash());
 
+//处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+    uploadDir: path.join(__dirname, 'public/img'), //上传文件目录
+    keepExtensions: true, //保留后缀
+}));
+
 //路由
 routes(app);
 
