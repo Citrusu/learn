a0b582e63b80c3b6da966feeab9b4576142bdf44
Citrus <zhouyou421@126.com>
2017-03-01 14:45:31 +0800

    done signin

--- myblog/lib/mongo.js
+++ myblog/lib/mongo.js
@@ -6,6 +6,9 @@ let Mongolass = require('mongolass');
 let mongolass = new Mongolass();
 mongolass.connect(config.mongodb);
 
+let moment = require('moment');
+let objectIdToTimestamp = require('objectid-to-timestamp');
+
 exports.User = mongolass.model('User',{
     name: {type: 'string'},
     password: {type: 'string'},
@@ -14,3 +17,19 @@ exports.User = mongolass.model('User',{
     bio: {type: 'string'}
 });
 exports.User.index({name: 1}, {unique: true}).exec();//根据用户名找到用户，用户名全局唯一
+
+// 根据id 生成创建时间 created_at
+mongolass.plugin('addCreatedAt',{
+    afterFind: (results) => {
+        results.forEach((item) => {
+            item.created_at = moment(objectIdToTimestamp(item._id)).format('YYYY-MM-DD HH:mm');
+        });
+        return results;
+    },
+    afterFindOne: (result) => {
+        if(result){
+            result.created_at = moment(objectIdToTimestamp(result._id)).format('YYYY-MM-DD HH:mm');
+        }
+        return result;
+    }
+});
