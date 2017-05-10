e439305d8a41faf4c0ade73bed4689ebcf74aec1
Citrus <zhouyou421@126.com>
2017-05-11 00:49:54 +0800

    base chat done

--- websocket/server.js
+++ websocket/server.js
@@ -6,14 +6,19 @@ const ws = require('ws').Server;
 
 const server = new ws({port:3000});
 
+const users = [];
+
 server.addListener('connection', function(socket){
     console.log('connection....');
-    console.log(socket);
+    users.push(socket);
 
     socket.addListener('message',function(msg){
         console.log(msg);
-
-        socket.send(msg);
+        users.forEach(function(n, i){
+            if(socket !== n){
+                n.send(msg);
+            }
+        });
     });
 });
 
