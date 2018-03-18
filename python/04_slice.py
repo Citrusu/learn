c895f95a438eeb7e3cf071b576e509f0f740ba5c
Citrus <zhouyou421@126.com>
2018-03-18 22:07:49 +0800

    up

---
+++ python/04_slice.py
@@ -0,0 +1,4 @@
+# 切片
+arr = ['a', 'b', 'c', 'd']
+ar = arr[-1]
+print(ar)
---
+++ python/05_input.py
@@ -0,0 +1,4 @@
+num1 = input('请输入第一个数: \n')
+num2 = input('请输入第二个数: \n')
+sum = int(num1) * int(num2)
+print('结果是：%s'%(sum))
