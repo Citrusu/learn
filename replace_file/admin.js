9bcea85dbfb25e4bc1d2acb8e029a8ff587ca98c
Citrus <zhouyou421@126.com>
2018-04-13 17:48:21 +0800

    up config

--- replace_file/admin.js
+++ replace_file/admin.js
@@ -9,22 +9,19 @@ module.exports = [
     [/<link rel="stylesheet" type="text\/css" href="__PUBLIC__\/Css\/reset.css">/g, ''],
 
     //通用
-    [/{\$NOW_THEME_URL}\/Assets\/Css\/common.css/g, '__STATIC__/admin/css/common.css'],
-    [/{\$NOW_THEME_URL}\/Assets\/Js\/common.js/g, '__STATIC__/admin/js/common.js'],
     [/__PUBLIC__\/Js\/jquery.min.js/g, '__STATIC__/admin/plugins/jquery/jquery.min.js'],
 
     //特殊
-    [/__PUBLIC__\/Plugins\/LhgDialog\/lhgdialog.js?skin=discuz/g, '__STATIC__/admin/plugins/lhgDialog/lhgdialog.js?skin=discuz'],
+    [/__PUBLIC__\/Plugins\/LhgDialog/g, '__STATIC__/admin/plugins/lhgDialog'],
 
     //变量
     [/<include file="Public/g, '<include file="pub'],
     [/__PUBLIC__\/Plugins\/Et/g, '__STATIC__/admin/plugins/et'],
-    // [/__PUBLIC__\/Plugins\/js/g, '__STATIC__/admin/plugins/js'],
     [/__PUBLIC__\/Plugins\//g, '__STATIC__/admin/plugins/'],
     [/__PUBLIC__\/Js/g, '__STATIC__/admin/js'],
-    [/{\$NOW_THEME_URL}\/Assets\/Css\//g, '__STATIC__/admin/css/'],
-    [/{\$NOW_THEME_URL}\/Assets\/Js\//g, '__STATIC__/admin/js/'],
-    [/{\$NOW_THEME_URL}\/Assets\/Images\//g, '__STATIC__/admin/js/'],
-    [/\{\$/g, '{\\$'],
+    [/{\$NOW_THEME_URL}\/Assets\/Css/g, '__STATIC__/admin/css'],
+    [/{\$NOW_THEME_URL}\/Assets\/Js/g, '__STATIC__/admin/js'],
+    [/{\$NOW_THEME_URL}\/Assets\/Images/g, '__STATIC__/admin/img'],
+    [/{\$/g, '{\\$'],
     [/{:U\(/g, '{:url('],
 ];
