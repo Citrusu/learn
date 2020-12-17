6f03f01b88ed26ef47e9413c6bc8acd295d186f1
citrus <zhouyou421@126.com>
2020-12-17 10:55:32 +0800

    up

---
+++ typescript/1.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var userName = 'Tom';
+console.log(sayHello(userName));
---
+++ typescript/1.ts
@@ -0,0 +1,6 @@
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let userName:string = 'Tom';
+console.log(sayHello(userName));
